3486f92f0f6f962f5240a2157a31465c7b709547
YunSeongWon1 <yso3996@naver.com>
2022-03-11 22:26:32 +0900

    added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1. Add the Class");
+		System.out.println("2. Delete the Class");
+		System.out.println("3. Edit the Class");
+		System.out.println("4. View the Class");
+		System.out.println("5. Exit");
+	}
+
+}
