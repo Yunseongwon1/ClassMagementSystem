ac1bdb7dccbfcfb1e502d74b418ddc5d7bc543f5
YunSeongWon1 <yso3996@naver.com>
2022-04-03 23:00:51 +0900

    added Classes

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,58 +1,33 @@
 import java.util.Scanner;
 
 public class MenuManager {
-	
-	public static void addClass() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Class Name:");
-		String className = input.next();
-		System.out.print("Professor Name:");
-		String professorName = input.next();
-		System.out.print("Classroom:");
-		String classroom = input.next();
-	}
-	public static void deleteClass() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Class Name:");
-		String className = input.next();
-	}
-	public static void editClass() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Class Name:");
-		String className = input.next();
-	}
-	public static void viewClass() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Class Name:");
-		String className = input.next();
-	}
 
 	public static void main(String[] args) {
-		int num = 0;
+		int num = -1;
 		Scanner input = new Scanner(System.in);
+		ClassManager classmanager = new ClassManager(input);
 		
-		while (num != 6) {
+		while (num != 5) {
 			System.out.println("*** Class Management System Menu ***");
 			System.out.println("1. Add the Class");
 			System.out.println("2. Delete the Class");
 			System.out.println("3. Edit the Class");
 			System.out.println("4. View the Class");
-			System.out.println("5. Show a menu");
-			System.out.println("6. Exit");
+			System.out.println("5. Exit");
 			System.out.print("Select one number between 1 - 6 :");
 			num = input.nextInt();
 			
 			if (num == 1) {
-				addClass();
+				classmanager.addClass();
 			}
 			else if (num == 2) {
-				deleteClass();
+				classmanager.deleteClass();
 			}
 			else if (num == 3) {
-				editClass();
+				classmanager.editClass();
 			}
 			else if (num == 4) {
-				viewClass();
+				classmanager.viewClass();
 			}
 		}
 	}
